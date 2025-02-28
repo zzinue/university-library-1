fcbbfcd58d258e3d6122820b96261bd6e5ffa10d
zzinue <zzinue@gmail.com>
2025-02-27 22:35:55 -0600

    addiing modifications from kubuntu

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -35,7 +35,7 @@ const BookCard = ({
                 height={18}
                 className="object-contain"
               />
-              <p className="text-light-100">11 days left to return</p>
+              <p className="text-light-100">10 days left to return</p>
             </div>
 
             <Button className="book-btn">Download receipt</Button>

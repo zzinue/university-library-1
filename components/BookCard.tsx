bbd9cf231df96a569cdf469d98d59ebebb08620e
zzinue <zzinue@gmail.com>
2025-04-12 21:08:02 -0600

    six days

--- components/BookCard.tsx
+++ components/BookCard.tsx
@@ -35,7 +35,7 @@ const BookCard = ({
                 height={18}
                 className="object-contain"
               />
-              <p className="text-light-100">07 days left to return</p>
+              <p className="text-light-100">06 days left to return</p>
             </div>
 
             <Button className="book-btn">Download receipt</Button>
